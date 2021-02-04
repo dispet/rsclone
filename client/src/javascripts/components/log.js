c8e2bba75a4938c1ace3a745823fb21a2a381f92
dispet <liftesk.@gmail.com>
2021-02-04 09:10:21 +0300

    feat: update log

--- client/src/javascripts/components/log.js
+++ client/src/javascripts/components/log.js
@@ -22,9 +22,7 @@ export class Log {
     get logtime() {
         const curr = this.currentTime.getTime();
         const createTime = new Date(this.createdAt).getTime();
-          // + (60000 * 60 * 3) + 10000;
         const elapsedTime = curr - createTime;
-        console.log(curr,createTime,elapsedTime);
         if (elapsedTime < minute)
             return (elapsedTime < 0) ? `${0} seconds ago`: `${Math.floor(elapsedTime/ second)} seconds ago`;
         if (elapsedTime < hour)
